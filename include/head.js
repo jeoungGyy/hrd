321242b946d6a6c195d55f82ccfcf5b19838b69d
Lim <limjungkoro@gmail.com>
2023-10-29 16:09:59 +0900

    1

--- include/head.js
+++ include/head.js
@@ -2,11 +2,11 @@ var str = '';
 
 str += '<meta http-equiv="X-UA-Compatible" content="IE=Edge">';
 str += '<meta name="viewport" content="width=640, user-scalable=yes">';
-str += '<link href="/new_css/reset.css?ver=0" rel="stylesheet">';
-str += '<link href="/new_css/pro.css" rel="stylesheet">';
-str += '<link href="/new_css/layout_m.css?ver=1" rel="stylesheet">';
-str += '<link href="/new_css/contents_m.css?ver=21" rel="stylesheet">';
-str += '<link href="/new_css/main_m.css?ver=0" rel="stylesheet">';
+str += '<link href="../new_css/reset.css?ver=0" rel="stylesheet">';
+str += '<link href="../new_css/pro.css" rel="stylesheet">';
+str += '<link href="../new_css/layout_m.css?ver=1" rel="stylesheet">';
+str += '<link href="../new_css/contents_m.css?ver=21" rel="stylesheet">';
+str += '<link href="../new_css/main_m.css?ver=0" rel="stylesheet">';
 
 
 document.write(str);
